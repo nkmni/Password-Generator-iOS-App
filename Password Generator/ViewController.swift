4bc5f5ab539d59fabb6cbf3721627ff259d65988
nkmni <7596222+nkmni@users.noreply.github.com>
2021-12-03 17:23:18 -0800

    Initial Commit

---
+++ Password Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password Generator
+//
+//  Created by Neil Khemani on 12/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
