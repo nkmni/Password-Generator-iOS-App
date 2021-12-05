e36639be3119ac9c935e2d2dc282f2d9c11e8a50
nkmni <7596222+nkmni@users.noreply.github.com>
2021-12-04 22:19:33 -0800

    Started "Generate" button functionality. Finished clipboard functionality.

--- Password Generator/ViewController.swift
+++ Password Generator/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var passwordDisplay: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func generateButtonPressed(_ sender: UIButton) {
+        passwordDisplay.text = ""
+        for i in 0..<6 {
+            var roll = ""
+            for _ in 0..<5 {
+                roll += "\(Int.random(in: 1...6))"
+            }
+            passwordDisplay.text += roll
+            if i < 5 {
+                passwordDisplay.text += "-"
+            }
+        }
+    }
 
+    @IBAction func copyButtonPressed(_ sender: UIButton) {
+        UIPasteboard.general.string = passwordDisplay.text
+    }
 }
 
